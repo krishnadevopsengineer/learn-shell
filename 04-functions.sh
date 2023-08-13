bc168d31d72ea1fa11d60937ef26637b89d1cb3c
krishna <krishna.rayanapati@gmail.com>
2023-08-13 13:54:38 +0530

    functions

--- 04-functions.sh
+++ 04-functions.sh
@@ -7,3 +7,17 @@ greeting(){
 
 greeting
 echo Function Exit Status - $?
+
+
+# You declare var in main program, you can access that in function
+# Function have its own special variables
+
+input() {
+  echo First Argument - $1
+  echo Second Argument - $2
+  echo All Argument - $*
+  echo No Of Argument - $#
+
+}
+
+input abc 1234
