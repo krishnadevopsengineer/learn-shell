4e42f478ccc788a2597e013ac6c600735444580f
krishna <krishna.rayanapati@gmail.com>
2023-08-13 12:20:49 +0530

    Vars

--- 03-vars.sh
+++ 03-vars.sh
@@ -1,2 +1,16 @@
+# Variables
 a=10
 echo a is $a
+
+# Special Variables
+# $0 - $N, $*, $#
+
+# Substituion Variables
+## Command Substituion
+DATE=$(date)
+
+echo Today date is $DATE
+
+## Arithmetic Substituion
+ADD=$(( 2+5 ))
+echo ADD of 2+5 = $ADD
